4d0e2190b3d658c2fc0a62e1d837024800310966
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-06-14 19:21:04 +0300

    Got rid of magic numbers

--- src/games/gameCalc.js
+++ src/games/gameCalc.js
@@ -4,9 +4,12 @@ const taskDescription = 'What is the result of the expression?';
 
 const ops = ['+', '-', '*'];
 
+const MIN_NUMBER = 0;
+const MAX_NUMBER = 100;
+
 const makeQuestion = () => {
-  const a = randInt(0, 100);
-  const b = randInt(0, 100);
+  const a = randInt(MIN_NUMBER, MAX_NUMBER);
+  const b = randInt(MIN_NUMBER, MAX_NUMBER);
   const op = ops[randInt(0, 3)];
   const question = `${a} ${op} ${b}`;
   let correctAnswer;
