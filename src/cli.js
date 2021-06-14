d86c556332a2fa0fb08f54873dfe47a193ee92bf
Alex Desyatnik <alexdesyatnik@outlook.com>
2021-06-14 18:48:19 +0300

    Moved game loop to cli.js

--- src/cli.js
+++ src/cli.js
@@ -7,9 +7,25 @@ const greetUser = () => {
   return name;
 };
 
-const runGame = (game) => {
+const gameLoop = (makeQuestion) => {
+  const MAX_ATTEMPTS = 3;
+  for (let attempt = 0; attempt < MAX_ATTEMPTS; attempt += 1) {
+    const { question, correctAnswer } = makeQuestion();
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Answer: ');
+    if (answer.toLowerCase() === correctAnswer) console.log('Correct!');
+    else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      return false;
+    }
+  }
+  return true;
+};
+
+const runGame = (taskDescription, makeQuestion) => {
   const name = greetUser();
-  const result = game();
+  console.log(taskDescription);
+  const result = gameLoop(makeQuestion);
   if (result) console.log(`Congratulations, ${name}!`);
   else console.log(`Let's try again, ${name}!`);
 };
--- src/gameEven.js
+++ src/gameEven.js
@@ -1,23 +1,14 @@
-import readlineSync from 'readline-sync';
+import randInt from './randInt.js';
 
-const randInt = (from, to) => Math.floor(Math.random() * (to - from) + from);
+const taskDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const MAX_ATTEMPTS = 3;
 const MIN_NUMBER = 0;
 const MAX_NUMBER = 100;
 
-export default () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let attempt = 0; attempt < MAX_ATTEMPTS; attempt += 1) {
-    const num = randInt(MIN_NUMBER, MAX_NUMBER);
-    const correctAnswer = (num % 2 === 0) ? 'yes' : 'no';
-    console.log(`Question: ${num}`);
-    const answer = readlineSync.question('Answer: ');
-    if (answer.toLowerCase() === correctAnswer) console.log('Correct!');
-    else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      return false;
-    }
-  }
-  return true;
+const makeQuestion = () => {
+  const question = randInt(MIN_NUMBER, MAX_NUMBER);
+  const correctAnswer = (question % 2 === 0) ? 'yes' : 'no';
+  return { question, correctAnswer };
 };
+
+export { taskDescription, makeQuestion };
